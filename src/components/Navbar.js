1fff9c430b5c15139536694f9c4a7c3501e77736
Jitender <jitendersharmateharki@gmail.com>
2023-06-24 12:36:54 +0530

    Added Logout and My orders button and the user accessibility according to login and logout butttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,59 @@
 import React from 'react'
 
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
 
 export default function Navbar() {
+  
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/login");
+  };
+  
+
   return (
     <>
         <nav className="navbar navbar-expand-lg bg-dark bg-body-tertiary">
-        <div className="container-fluid navbar-dark">
+        <div className="container-fluid navbar-dark me-auto">
             <Link className="navbar-brand" to="#">FoodApp</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav me-auto">
                 <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                 </li>
-                <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link>
+            {
+              (localStorage.getItem("authToken"))
+              ?<li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to="/">My Orders</Link>
                 </li>
-                <li className="nav-item">
-                <Link className="nav-link" to="/signup">Signup</Link>
-                </li>
+              :""
+            }
             </ul>
+
+            {
+              (!localStorage.getItem("authToken"))
+              ?<div className='d-flex'>
+                <Link className="nav-link" to="/login">Login</Link>
+                <Link className="nav-link" to="/signup">Signup</Link>
+              </div>
+              :<div className='d-flex'>
+                <div>
+                  {/* <Link className="nav-link" to="/login" >LogOUt</Link> */}
+                  {/* but we need to handle to authToken also */}
+                  <div className='btn bg-white text-dark mx-2' onClick={handleLogout}>My Cart</div>
+                </div>
+                <div>
+                  {/* <Link className="nav-link" to="/login" >LogOUt</Link> */}
+                  {/* but we need to handle to authToken also */}
+                  <div className='btn bg-white text-danger mx-2' onClick={handleLogout}>LogOUt</div>
+                </div>
+              </div>
+            }
             </div>
         </div>
         </nav>
